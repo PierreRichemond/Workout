7766c5aa0406a5678c37296ca67cbdbac4a48aa4
= <pierreambroise@hotmail.fr>
2022-02-04 16:56:09 +0900

    added validation to pass the exercises spec

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -4,15 +4,12 @@ RSpec.feature 'Creating exercise' do
   before do
     @john = User.create!(email: 'john@example.com', password: 'password')
     login_as(@john)
-  end
-  scenario 'with valid input' do
     visit '/'
-
     click_link 'My Lounge'
     click_link 'New Workout'
-
     expect(page).to have_link('Back')
-
+  end
+  scenario 'with valid input' do
     fill_in 'Duration (min)', with: 70
     fill_in 'Workout details', with: 'Weight lifting'
     fill_in 'Activity date', with: '2022-02-15'
@@ -24,5 +21,17 @@ RSpec.feature 'Creating exercise' do
     expect(current_path).to eq(user_exercise_path(@john, exercise))
     expect(exercise.user_id).to eq(@john.id)
 
+  end
+
+  scenario "with invalid inputs" do
+   fill_in "Duration", with: ""
+   fill_in "Workout details",  with: ""
+   fill_in "Activity date",  with: ""
+   click_button "Create Exercise"
+
+   expect(page).to have_content("Exercise has not been created")
+   expect(page).to have_content("Duration in min is not a number")
+   expect(page).to have_content("Workout details can't be blank")
+   expect(page).to have_content("Activity date can't be blank")
   end
 end
