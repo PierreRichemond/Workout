19667e7a8c18ed4774a2288918877fd7ca2b56c9
= <pierreambroise@hotmail.fr>
2022-02-04 15:39:17 +0900

    created the controller and route for the exercise

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   get 'home/index'
+  resources :users do
+    resources :exercises
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
