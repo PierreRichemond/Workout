16184505b2308eb645b3115c6548467da5be3136
= <pierreambroise@hotmail.fr>
2022-02-08 15:10:43 +0900

    changed the webpack config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'environment'
 
-const { environment } = require('@rails/webpacker')
+const { environment } = require('./environment')
 
 module.exports = environment
